df8ec79b9c815c85f5f4fd4e0998852c1217629a
Richard Fuchs <rfuchs@sipwise.com>
2024-02-05 12:57:40 -0500

    MT#55283 refactor timer thread creation
    Use a unified function to launch timer threads.
    
    Functional no-op.
    
    Change-Id: I6debd71220cfe464c48c00e5d194b5cf9b65e16c

--- include/codec.h
+++ include/codec.h
@@ -92,7 +92,7 @@ struct codec_scheduler {
 
 void codecs_init(void);
 void codecs_cleanup(void);
-void codec_timers_loop(void *);
+void codec_timers_launch(void);
 void rtcp_timer_stop(struct rtcp_timer **);
 void codec_timer_callback(call_t *, void (*)(call_t *, void *), void *, uint64_t delay);
 
